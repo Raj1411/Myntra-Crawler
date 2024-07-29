f656c8ea7e044b32d1d01c2df97cd11dbb0338ad
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2024-07-29 13:28:39 +0530

    app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
-import json
 import requests as rq
 
 app = Flask(__name__)
@@ -9,7 +8,7 @@ headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 }
 
-def get_price(style_id):
+def get_soup(style_id):
     url = f'https://www.myntra.com/foundation-and-primer/swiss-beauty/swiss-beauty-long-lasting-makeup-fixer-natural-spray---aloe-vera-with-vitamin-e-50-ml/{style_id}/buy'
     
     try:
@@ -17,22 +16,13 @@ def get_price(style_id):
         res.raise_for_status()  # Check for HTTP request errors
         
         soup = BeautifulSoup(res.text, 'html.parser')
-        script_text = next((s.get_text(strip=True) for s in soup.find_all("script") if 'pdpData' in s.text), None)
-        
-        if script_text:
-            try:
-                data = json.loads(script_text[script_text.index('{'):])
-                mrp = data['pdpData']['price']['mrp']
-                return data
-            except (json.JSONDecodeError, KeyError):
-                pass
-        return 'OOS'
+        return str(soup)  # Convert soup to a string
         
     except rq.RequestException:
         return 'OOS'
 
-@app.route('/get_prices', methods=['GET'])
-def get_prices():
+@app.route('/get_soup', methods=['GET'])
+def get_soup_view():
     style_ids = request.args.get('style_ids')
     if not style_ids:
         return jsonify({'error': 'style_ids parameter is required'}), 400
@@ -40,8 +30,8 @@ def get_prices():
     style_ids = style_ids.split(',')
     data = []
     for style_id in style_ids:
-        mrp = get_price(style_id)
-        data.append({'style_id': style_id, 'mrp': mrp})
+        soup = get_soup(style_id)
+        data.append({'style_id': style_id, 'soup': soup})
     
     return jsonify(data)
 
