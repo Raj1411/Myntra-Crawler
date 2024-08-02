11bdcebe210673d1a38a4749cdbb1a7467e82479
Raj1411 <56500451+Raj1411@users.noreply.github.com>
2024-08-02 20:09:34 +0530

    app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def get_page_source(url):
     headers = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36" }
 
     try:
-        driver.get(url,headers=headers)
+        driver.get(url)
         page_source = driver.page_source
     finally:
         driver.quit()
